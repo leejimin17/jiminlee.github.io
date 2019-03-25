2b32c263f985862f82a79f3a5d449194e614213a
leejimin <you@example.com>
2019-03-25 20:25:14 +0900

    firsr commit

---
+++ practice.py
@@ -0,0 +1,15 @@
+likelion=5
+
+while likelion <7:
+    print("wooooo")
+    likelion = likelion+1
+    if likelion ==7:
+        print("yehhhhhhhh")
+
+
+test=['one','two','three']
+for i in test:
+    print(i)
+
+lionweknow =['a','s','f'.'g'.'h']
+lionmeet=['a','s','']
